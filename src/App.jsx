ed65d5437d5b6e75caced7c56bc551abe4753a1d
Suraj Chaudhary <isurajcdry@gmail.com>
2025-06-30 09:52:24 -0400

    added functionality to Dice and random generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Die from './components/Die'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [dice, setDice] = useState(generateAllNewDice())
+
+  function generateAllNewDice() {
+    let randomNums = []
+
+    for (let i = 0; i < 10; i++) {
+      randomNums.push(Math.ceil(Math.random() * 6))
+    }
+
+    return randomNums
+  }
+
+  function rollDice() {
+    setDice(generateAllNewDice())
+  }
+
+  const eachDie = dice.map((val) => <Die value={val} />)
 
   return (
     <>
-      <p>Hello</p>
+      <main>
+        <div className="dice-container">
+          {eachDie}
+        </div>
+
+        <button className="roll-dice" onClick={rollDice}>Roll</button>
+      </main>
     </>
   )
 }
